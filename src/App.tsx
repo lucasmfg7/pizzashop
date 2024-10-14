4484d1e8b26b03aa806132c6566e5cca11749df4
Lucas Fernandes <lucas_matheus22@hotmail.com>
2024-10-13 21:04:35 -0300

    Instalando shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+import "./index.css";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
+  );
 };
